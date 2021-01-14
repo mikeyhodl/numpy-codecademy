e87cd7f2ce1698359055e67c44dbacf784f5a4b1
Mike Owino <mike.owino40@yahoo.com>
2021-01-14 14:39:50 +0300

    Create Operations with NumPy Arrays.py

---
+++ Operations with NumPy Arrays.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+test_1 = np.array([92, 94, 88, 91, 87])
+test_2 = np.array([79, 100, 86, 93, 91])
+test_3 = np.array([87, 85, 72, 90, 92])
+
+test_3_fixed = test_3 + 2
+
+print(test_3_fixed)
