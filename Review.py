593403968f908944253a46749fe42982929a37f1
Mike Owino <mike.owino40@yahoo.com>
2021-01-14 17:47:28 +0300

    Update Review.py

--- Review.py
+++ Review.py
@@ -3,13 +3,19 @@ import numpy as np
 temperatures = np.genfromtxt('temperature_data.csv', delimiter=',')
 
 print(temperatures)
-
+print("")
 temperatures_fixed = temperatures + 3
 
 print(temperatures_fixed)
-
+print("")
 monday_temperatures = temperatures_fixed[0,:]
+print(monday_temperatures)
+print("")
 
 thursday_friday_morning = temperatures_fixed[3:5,1]
+print(thursday_friday_morning)
+print("")
 
 temperature_extremes = temperatures_fixed[(temperatures_fixed < 50) | (temperatures_fixed > 60)]
+
+print(temperature_extremes)
