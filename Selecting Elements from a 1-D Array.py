9fbf65df4aba849a7e2c3990eea9ec2bd271d142
Mike Owino <mike.owino40@yahoo.com>
2021-01-14 15:13:39 +0300

    Create Selecting Elements from a 1-D Array.py

---
+++ Selecting Elements from a 1-D Array.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+test_1 = np.array([92, 94, 88, 91, 87])
+test_2 = np.array([79, 100, 86, 93, 91])
+test_3 = np.array([87, 85, 72, 90, 92])
+
+jeremy_test_2 = test_2[-2]
+
+manual_adwoa_test_1 = test_1[1:3]
